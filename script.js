9f52538adae75fd43b660aa23e86fde6c5fd5f19
Francis Okorie <92164567+Francis-Okorie@users.noreply.github.com>
2025-01-08 22:15:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const budgetInput = document.querySelector(".budget-value");
 const budgetNumber =document.querySelector(".budget-number");
 const budgetAddBtn = document.querySelector(".budget-add-btn");
 const budgetContainer = document.querySelector(".budget-input");
+const productAddBtn = document.querySelector(".main-add-btn");
+const productNameInput =document.querySelector(".product-name-input");
+const productPriceInput = document.querySelector(".product-price-input");
+const budgetDetailContainer = document.querySelector(".budget-detail-container");
 
 let totalValue = 0;
+let totalProductPrice = 0;
 
 budgetAddBtn.addEventListener("click", ()=>{
     let newBudgetInput = parseFloat(budgetInput.value.trim());
@@ -22,3 +27,37 @@ budgetAddBtn.addEventListener("click", ()=>{
     
     budgetNumber.textContent = totalValue;
 });
+
+productAddBtn.addEventListener("click", ()=>{
+    let newProductName = productNameInput.value.trim();
+    let newProductPrice = productPriceInput.value.trim();
+
+    let displayDetail = document.createElement("div");
+    displayDetail.setAttribute("class", "display-details")
+
+    let productNameElement = document.createElement("p");
+    let productPriceElement = document.createElement("p");
+    let editBtn = document.createElement("img");
+    let deleteBtn = document.createElement("img");
+
+    productNameElement.innerHTML = newProductName;
+    productPriceElement.innerHTML = newProductPrice;
+    editBtn.src = 'images/pencil-square.svg';
+    deleteBtn.src = 'images/trash3-fill.svg';
+
+    displayDetail.appendChild(productNameElement);
+    displayDetail.appendChild(productPriceElement);
+    displayDetail.appendChild(editBtn);
+    displayDetail.appendChild(deleteBtn);
+    budgetDetailContainer.appendChild(displayDetail);
+
+    totalProductPrice += newProductPrice;
+    updateTotalDisplay();
+    
+});
+
+
+function updateTotalDisplay() {
+    const totalDisplayElement = document.querySelector(".total-expense-value");
+    totalDisplayElement.innerHTML = `$${totalProductPrice}`;
+}
